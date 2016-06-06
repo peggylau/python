f7e29027f02cf238fece5a175898fc5767094e57
peggylau <peggylau@163.com>
2016-06-06 13:22:19 +0800

    change config.py

--- weixin/nagios_alert/config.py
+++ weixin/nagios_alert/config.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 
-CorpID='wxbeb81f6dcdf3babe'
-Secret='MSf19qLWl75FIld26crfTLigdjq9EDVPwYDN9kQEYl8RKHqwW_4NI2HEkzrWJOu1'
+CorpID=''
+Secret=''
 
-ToUser='wangyou'
+ToUser='user1'
 AgentId=1
